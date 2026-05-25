7e30e7626aba0660f84203c827915f66e8196637
keshavvijay87 <keshavvijay887@gmail.com>
2026-05-25 14:34:40 +0530

    7commit

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const product = [];
+const products = [];
 
 const productForm = document.querySelector("#product-form");
 
 productForm.addEventListener("submit" ,function(event) {
 
-    console.log("submitted");
     event.preventDefault();
+
+    const name = document.querySelector("#productName").value;
+    if( name=== "")
+
+    const quantity = document.querySelector("#productQuantity").value;
+
+    const price = document.querySelector("#productPrice").value;
+
+    console.log(name);
+    console.log(quantity);
+    console.log(price);
 });
